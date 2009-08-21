08d95ad8617c6b659e97060ea65c7d834bad2b46
Daniel Franke <franke@embl-hamburg.de>
2009-08-21 08:40:50 +0000

    2009-08-21  Daniel Franke  <dfranke@users.sourceforge.net>
    	* saxsdocument_format.h: Add license.

--- libsaxsdocument/saxsdocument_format.h
+++ libsaxsdocument/saxsdocument_format.h
@@ -1,4 +1,25 @@
-
+/*
+ * Format handling of SAXS documents.
+ * Copyright (C) 2009 Daniel Franke <dfranke@users.sourceforge.net>
+ *
+ * This file is part of libsaxsdocument.
+ *
+ * libsaxsdocument is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * libsaxsdocument is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with libsaxsdocument. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef LIBSAXSDOCUMENT_SAXSDOCUMENT_FORMAT_H
 #define LIBSAXSDOCUMENT_SAXSDOCUMENT_FORMAT_H
