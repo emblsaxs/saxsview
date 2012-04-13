8e10957d7514d65245ad4d66425e806a34ff0ef7
Daniel Franke <franke@embl-hamburg.de>
2012-04-13 20:08:43 +0000

    2012-04-13  Daniel Franke  <dfranke@users.sourceforge.net>
    	* DeployQt4.cmake: Fix cmake bug #13129.
    	* Windows/install.cmake (saxsview_install_dependencies): Implemented.
    	* Windows/packaging.cmake: Defined NSIS specific variables.

--- cmake/Windows/install.cmake
+++ cmake/Windows/install.cmake
@@ -1,5 +1,37 @@
 
-macro (saxsview_install_dependencies)
-  # TODO
-endmacro (saxsview_install_dependencies)
+include (GNUInstallDirs)
 
+macro (saxsview_install_dependencies)
+  file (GLOB PLUGINS "${QT_PLUGINS_DIR}/imageformats/*.dll")
+  install (CODE "set (CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake ${CMAKE_MODULE_PATH})
+
+                 #
+                 # It is necessary to evaluate this expression at runtime, otherwise
+                 # `make install`would work, but not `make packge`.
+                 #
+                 set (PREFIX \$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_BINDIR})
+
+                 # 
+                 # The local copy of BundleUtilities.cmake includes a workaround for:
+                 #   http://www.cmake.org/Bug/view.php?id=13052
+                 #
+                 include(BundleUtilities)
+
+                 # 
+                 # The local copy of DeployQt4.cmake includes fixes for:
+                 #   http://www.cmake.org/Bug/view.php?id=13051
+                 #   http://www.cmake.org/Bug/view.php?id=13129
+                 #
+                 include(DeployQt4)
+
+				 #
+				 # There is a difference between ´make install` and
+				 # `make package` - the latter installs into a different
+				 # location ...
+				 #
+                 FIXUP_QT4_EXECUTABLE (\"\${PREFIX}/${ARGV0}.exe\"
+                                       \"${PLUGINS}\"
+                                       \"\"
+                                       \"\${PREFIX}\"
+                                       \"plugins\")")
+endmacro (saxsview_install_dependencies)
--- cmake/Windows/packaging.cmake
+++ cmake/Windows/packaging.cmake
@@ -1,3 +1,20 @@
 
-set (CPACK_GENERATOR   NSIS)
-set (CPACK_STRIP_FILES TRUE)
+set (CPACK_GENERATOR              NSIS ZIP)
+set (CPACK_STRIP_FILES            TRUE)
+
+set (CPACK_NSIS_DISPLAY_NAME      "${CPACK_PACKAGE_NAME} ${CPACK_PACKAGE_VERSION}")
+set (CPACK_NSIS_PACKAGE_NAME      "${CPACK_PACKAGE_NAME} ${CPACK_PACKAGE_VERSION}")
+set (CPACK_NSIS_CONTACT           ${CPACK_PACKAGE_CONTACT})
+set (CPACK_NSIS_URL_INFO_ABOUT    "http://saxsview.sourceforge.net")
+
+# set (CPACK_NSIS_MUI_ICON        ${CMAKE_CURRENT_SOURCE_DIR}/saxsview.ico)
+# set (CPACK_NSIS_MUI_UNIICON     ${CMAKE_CURRENT_SOURCE_DIR}/saxsview.ico)
+
+# Links to external web pages in the Start menu
+set (CPACK_NSIS_MENU_LINKS        COPYING.txt "SaxsView License"
+                                  "http://saxsview.sourceforge.net" "saxsview Homepage")
+
+# An extra page will appear in the installer that will allow the 
+# user to choose whether the program directory should be added
+# to the system PATH variable.
+set (CPACK_NSIS_MODIFY_PATH       ON)
