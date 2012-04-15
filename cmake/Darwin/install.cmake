b5edda8b5d89f08b23ac124a3e575ac0c5d35cb1
Daniel Franke <franke@embl-hamburg.de>
2012-04-15 19:32:13 +0000

    2012-04-15  Daniel Franke  <dfranke@users.sourceforge.net>
    	* Darwin/packaging.cmake: Define CPACK_PACKAGING_INSTALL_PREFIX.
    	* Darwin/install.cmake (saxsview_install_dependencies): Fixed packaging
    	and plugin install location.

--- cmake/Darwin/install.cmake
+++ cmake/Darwin/install.cmake
@@ -1,10 +1,15 @@
 
 
 macro (saxsview_install_dependencies)
-  set (PREFIX $ENV{DESTDIR}${CMAKE_INSTALL_PREFIX})
-  file (GLOB PLUGINS "${QT_PLUGINS_DIR}/imageformats/*")
-
+  file (GLOB PLUGINS "${QT_PLUGINS_DIR}/imageformats/*.dylib")
   install (CODE "set (CMAKE_MODULE_PATH ${CMAKE_SOURCE_DIR}/cmake ${CMAKE_MODULE_PATH})
+
+                 #
+                 # It is necessary to evaluate this expression at runtime, otherwise
+                 # `make install`would work, but not `make packge`.
+                 #
+                 set (PREFIX \$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX})
+
                  #
                  # Implement a path override used in GetPrerequisites/BundleUtilities
                  # to specify the Framework search path relative to the location of the
@@ -16,20 +21,26 @@ macro (saxsview_install_dependencies)
 
                  # 
                  # The local copy of BundleUtilities.cmake includes a workaround for:
-                 #   http://public.kitware.com/Bug/view.php?id=13052
+                 #   http://www.cmake.org/Bug/view.php?id=13052
                  #
                  include(BundleUtilities)
 
                  # 
-                 # The local copy of DeployQt4.cmake includes the fix for:
-                 #   http://public.kitware.com/Bug/view.php?id=13051
+                 # The local copy of DeployQt4.cmake includes fixes for:
+                 #   http://www.cmake.org/Bug/view.php?id=13051
+                 #   http://www.cmake.org/Bug/view.php?id=13129
                  #
                  include(DeployQt4)
 
-                 FIXUP_QT4_EXECUTABLE (\"${PREFIX}/${ARGV0}.app\"
+                 #
+                 # The location of the plugins is assumed to be relative 
+                 # to the bundle contents. See also:
+                 #   http://www.cmake.org/Bug/view.php?id=13133
+                 #
+                 FIXUP_QT4_EXECUTABLE (\"\${PREFIX}/${ARGV0}.app\"
                                        \"${PLUGINS}\"
                                        \"\"
-                                       \"${PREFIX}/Frameworks\"
-                                       \"${PREFIX}/Frameworks/Plugins\")")
+                                       \"\${PREFIX}/Frameworks\"
+                                       \"../../Frameworks/Plugins\")")
 
 endmacro (saxsview_install_dependencies)
--- cmake/Darwin/packaging.cmake
+++ cmake/Darwin/packaging.cmake
@@ -1,2 +1,5 @@
 
-set (CPACK_GENERATOR  "PackageMaker")
+set (CPACK_GENERATOR                 "PackageMaker")
+
+# Required, otherwise the package will install itself in '/usr' by default.
+set (CPACK_PACKAGING_INSTALL_PREFIX  ${CMAKE_INSTALL_PREFIX})
