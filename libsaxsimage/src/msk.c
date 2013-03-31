b9cd56cb58ab933939c95ff2cb489acafefa187d
Daniel Franke <franke@embl-hamburg.de>
2013-03-31 22:16:10 +0000

    2013-04-01  Daniel Franke  <dfranke@users.sourceforge.net>
    	* src/msk.c (saxs_image_format_msk): Registered writer; moved magic
    	check to ...
    	(saxs_image_msk_read): ... here.
    	(saxs_image_msk_write): New.

--- libsaxsimage/src/msk.c
+++ libsaxsimage/src/msk.c
@@ -1,6 +1,6 @@
 /*
- * Read files in Fit2D .msk format.
- * Copyright (C) 2011 Daniel Franke <dfranke@users.sourceforge.net>
+ * Read and write files in Fit2D .msk format.
+ * Copyright (C) 2011, 2013 Daniel Franke <dfranke@users.sourceforge.net>
  *
  * This file is part of libsaxsimage.
  *
@@ -28,22 +28,33 @@
 #include <limits.h>
 #include <errno.h>
 #include <string.h>
+#include <math.h>
+
+#ifndef DBL_EPSILON
+#define DBL_EPSILON 1e-16
+#endif
 
 
 int saxs_image_msk_read(saxs_image *image, const char *filename) {
+  int magic[4] = { 0 };
   int width, height, padding;
   int row, col, bit, tmp;
 
   FILE *fd = fopen(filename, "rb");
-  if (!fd) {
-    printf("%s: %s\n", filename, strerror(errno));
-    return -1;
-  }
+  if (!fd)
+    return errno;
+
+  /* Check the magic header. */
+  fread(magic, sizeof(magic), 1, fd);
 
   /*
-   * Skip the identifier (see saxs_image_format_msk()).
+   * The first four times four bytes shall contain
+   * the characters 'MASK':
    */
-  fseek(fd, 16, SEEK_SET);
+  if (magic[0] != 'M' || magic[1] != 'A' || magic[2] != 'S' || magic[3] != 'K') {
+    fclose(fd);
+    return ENOTSUP;
+  }
 
   /*
    * Next three four-byte blocks are width, height
@@ -77,35 +88,60 @@ int saxs_image_msk_read(saxs_image *image, const char *filename) {
 }
 
 
+int saxs_image_msk_write(saxs_image *image, const char *filename) {
+  const int magic[4] = { 'M', 'A', 'S', 'K' };
+
+  int width, height, padding;
+  int row, col, bit;
+
+  FILE *fd = fopen(filename, "wb");
+  if (!fd)
+    return errno;
+
+  width   = saxs_image_width(image);
+  height  = saxs_image_height(image);
+  padding = sizeof(int) * (width / sizeof(int) + 1) - width;
+
+  /* Header: any endianess issues here? */
+  fwrite(magic, sizeof(magic), 1, fd);
+  fwrite(&width, sizeof(width), 1, fd);
+  fwrite(&height, sizeof(height), 1, fd);
+  fwrite(&padding, sizeof(padding), 1, fd);
+
+  /* Data starts with an offset of 1024 bytes. */
+  fseek(fd, 1024, SEEK_SET);
+
+  for (row = 0; row < height; ++row)
+    for (col = 0; col < width; col += sizeof(int) * CHAR_BIT) {
+      int tmp = 0;
+
+      for (bit = 0; (unsigned)bit < sizeof(tmp) * CHAR_BIT && col + bit < width; ++bit) {
+        double value = saxs_image_value(image, col + bit, row);
+        if (fabs(value) > DBL_EPSILON)
+          tmp |= (1 << bit);
+      }
+
+      fwrite(&tmp, sizeof(tmp), 1, fd);
+    }
+
+  /* Any footer? */
+
+  fclose(fd);
+  return 0;
+}
+
+
 /**************************************************************************/
 #include "saxsimage_format.h"
 
 saxs_image_format*
 saxs_image_format_msk(const char *filename, const char *format) {
-  static saxs_image_format image_msk = { saxs_image_msk_read, NULL };
-
-  int header[4];
+  static saxs_image_format image_msk = { saxs_image_msk_read,
+                                         saxs_image_msk_write };
 
   if (!compare_format(format, "msk")
-      || !compare_format(suffix(filename), "msk")) {
-
-    FILE *fd = fopen(filename, "rb");
-    if (!fd)
-      return NULL;
-
-    fread(header, sizeof(int), 4, fd);
-    fclose(fd);
-
-    /*
-     * The first four times four bytes shall contain
-     * the characters 'MASK':
-     */
-    if (   header[0] == 'M'
-        && header[1] == 'A'
-        && header[2] == 'S'
-        && header[3] == 'K')
-      return &image_msk;
-  }
+      || !compare_format(suffix(filename), "msk"))
+    return &image_msk;
 
   return NULL;
 }
