2f143779ce46ee8c03f124f5f8051b9c434b81eb
Chris Kerr <ckerr@embl-hamburg.de>
2015-03-18 16:41:35 +0000

    2015-03-18  Chris Kerr  <cjkerr@users.sourceforge.net>
    	* libsaxsdocument/readtest.c: New program for very simple tests
    	* libsaxsdocument/CMakeLists.txt: Added a test (currently failing) that non-SAXS files should be rejected

---
+++ testsuite/libsaxsdocument/readtest.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#include "saxsdocument.h"
+
+int main(int argc, char** argv){
+  char *input = (argc > 1) ? argv[1] : "-";
+  printf("Reading '%s'\n", input);
+
+  saxs_document *doc = saxs_document_create();
+  int result = saxs_document_read(doc, input, NULL);
+  printf("Result: %d (%s)\n", result, strerror(result));
+  if (result == 0) {
+    printf("Number of properties: %d\n", saxs_document_property_count(doc));
+    printf("Number of curves: %d\n", saxs_document_curve_count(doc));
+  }
+  saxs_document_free(doc);
+
+}
