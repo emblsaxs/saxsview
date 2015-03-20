640b60a472df571374cf91e8ad61d086a835b12b
Chris Kerr <ckerr@embl-hamburg.de>
2015-03-20 14:44:43 +0000

    2015-03-20  Chris Kerr  <cjkerr@users.sourceforge.net>
    		* src/saxsproperty.c: Check for memory allocation errors, add assertions that property lists are valid

--- libsaxsdocument/src/saxsproperty.c
+++ libsaxsdocument/src/saxsproperty.c
@@ -25,6 +25,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 struct saxs_property {
   char *name, *value;
@@ -36,12 +37,56 @@ struct saxs_property_list {
   struct saxs_property *head, *tail;
 };
 
+#ifdef NDEBUG
+
+#define assert_valid_property(p)
+#define assert_valid_property_or_null(p)
+#define assert_valid_property_list(plist)
+#define assert_valid_property_list_or_null(plist)
+
+#else
+
+#define assert_valid_property(p) { \
+  assert(p != NULL); \
+  assert(p->name != NULL); \
+  assert(p->value != NULL); \
+}
+
+#define assert_valid_property_or_null(p) { \
+  if(p) assert_valid_property(p); \
+}
+
+static void assert_valid_property_list(const struct saxs_property_list *plist) {
+  assert(plist != NULL);
+  assert(plist->count >= 0);
+  if (plist->count == 0) {
+    assert(plist->head == NULL);
+    assert(plist->tail == NULL);
+  } else {
+    assert_valid_property(plist->head);
+    assert_valid_property(plist->tail);
+    assert(plist->tail->next == NULL);
+    int cnt = 1;
+    const saxs_property *p = plist->head;
+    for(;p != plist->tail; p = p->next) {
+      assert_valid_property(p);
+      ++cnt;
+    }
+    assert(cnt == plist->count);
+  }
+}
+
+#define assert_valid_property_list_or_null(plist) { \
+  if(plist) assert_valid_property_list(plist); \
+}
+
+#endif
 
 saxs_property*
 saxs_property_create(const char *name, const char *value) {
   saxs_property *property;
 
-  if (!name)
+  if (!name || !value)
     return NULL;
 
   property = malloc(sizeof(saxs_property));
@@ -49,18 +94,28 @@ saxs_property_create(const char *name, const char *value) {
     property->name = strdup(name);
     property->value = strdup(value);
     property->next = NULL;
+    if (property->name == NULL || property->value == NULL) {
+      free(property->name);
+      free(property->value);
+      free(property);
+      property = NULL;
+    }
   }
 
+  assert_valid_property_or_null(property);
   return property;
 }
 
 saxs_property*
 saxs_property_next(const saxs_property *property) {
+  assert_valid_property_or_null(property);
   return property ? property->next : NULL;
 }
 
 saxs_property*
 saxs_property_find_next(const saxs_property *property, const char *name) {
+  assert_valid_property_or_null(property);
+
   saxs_property* nextproperty = property ? property->next : NULL;
   while (nextproperty && (strcmp(nextproperty->name, name) != 0))
     nextproperty = nextproperty->next;
@@ -70,16 +125,19 @@ saxs_property_find_next(const saxs_property *property, const char *name) {
 
 const char*
 saxs_property_name(const saxs_property *property) {
+  assert_valid_property_or_null(property);
   return property ? property->name : NULL;
 }
 
 const char*
 saxs_property_value(const saxs_property *property) {
+  assert_valid_property_or_null(property);
   return property ? property->value : NULL;
 }
 
 void
 saxs_property_free(saxs_property *property) {
+  assert_valid_property_or_null(property);
   if (property) {
     if (property->name)
       free(property->name);
@@ -99,11 +157,14 @@ saxs_property_list_create() {
     list->head = NULL;
     list->tail = NULL;
   }
+  assert_valid_property_list_or_null(list);
   return list;
 }
 
 void
 saxs_property_list_insert(saxs_property_list *list, saxs_property *property) {
+  assert_valid_property_list_or_null(list);
+  assert_valid_property_or_null(property);
   if (list && property) {
     if (!list->head)
       list->head = property;
@@ -117,16 +178,19 @@ saxs_property_list_insert(saxs_property_list *list, saxs_property *property) {
 
 int
 saxs_property_list_count(const saxs_property_list *list) {
+  assert_valid_property_list_or_null(list);
   return list ? list->count : 0;
 }
 
 saxs_property*
 saxs_property_list_first(const saxs_property_list *list) {
+  assert_valid_property_list_or_null(list);
   return list ? list->head : NULL;
 }
 
 saxs_property*
 saxs_property_list_find_first(const saxs_property_list *list, const char *name) {
+  assert_valid_property_list_or_null(list);
   saxs_property *property = saxs_property_list_first(list);
 
   while (property && strcmp(saxs_property_name(property), name) != 0)
@@ -137,6 +201,7 @@ saxs_property_list_find_first(const saxs_property_list *list, const char *name) 
 
 void
 saxs_property_list_free(saxs_property_list *list) {
+  assert_valid_property_list_or_null(list);
   while (list && list->head) {
     saxs_property *property = list->head;
     list->head = list->head->next;
