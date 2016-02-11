8a87f7bc53c6526d43e061ace71fd98ccc4d311c
Chris Kerr <ckerr@embl-hamburg.de>
2016-02-11 10:57:07 +0100

    Test pysaxsdocument's reading of curves and properties

--- testsuite/pysaxsdocument/test_pysaxsdocument.py
+++ testsuite/pysaxsdocument/test_pysaxsdocument.py
@@ -7,8 +7,22 @@ import saxsdocument
 
 class TestSAXSDocument(unittest.TestCase):
 
-    def test_read(self):
-        saxsdocument.read("bsa.dat")
+    def setUp(self):
+        self.doc = saxsdocument.read("bsa.dat")
+
+    def test_curves(self):
+        curves = self.doc.curve
+        self.assertIsInstance(curves, list)
+        self.assertEqual(len(curves), 1)
+        self.assertIsInstance(curves[0], list)
+        self.assertEqual(len(curves[0]), 2096)
+        self.assertTupleEqual(curves[0][0],
+                              (0.741270E-01, 0.260465E+05, 0.321129E+02))
+
+    def test_properties(self):
+        properties = self.doc.property
+        self.assertIsInstance(properties, dict)
+        self.assertEqual(properties["Sample code"], "BSA")
 
 
 if __name__ == "__main__":
