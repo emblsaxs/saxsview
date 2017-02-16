8af750f81800405297313b1e04e20f979631d8d2
Chris Kerr <ckerr@embl-hamburg.de>
2017-02-16 09:50:43 +0100

    Changed the readtest program to try all formats
    i.e. rather than stopping on the first success
    
    Updated the expected test output to match

--- testsuite/libsaxsdocument/readtest.c
+++ testsuite/libsaxsdocument/readtest.c
@@ -1,32 +1,83 @@
 
+#include <assert.h>
 #include <stdio.h>
 #include <string.h>
 
-#include "saxsdocument.h"
+#include <columns.h>
+#include <saxsdocument.h>
+#include <saxsdocument_format.h>
 
 int main(int argc, char** argv){
   char *input = (argc > 1) ? argv[1] : "-";
   char *format = (argc > 2) ? argv[2] : NULL;
+  assert(input);
+
+  struct saxs_document_format const *fmt1;
+  if (format) {
+    fmt1 = saxs_document_format_find_first(input, format);
+    if (!fmt1) {
+      printf("No format available for filename '%s', format '%s'\n", input, format);
+      return 0;
+    }
+  } else {
+    fmt1 = saxs_document_format_first();
+  }
+  assert(fmt1);
 
 #ifdef __AFL_HAVE_MANUAL_CONTROL
   while (__AFL_LOOP(1000)) {
 #else
-  printf("Reading '%s'\n", input);
+  if (format) {
+    printf("Reading '%s' with format '%s'...\n", input, format);
+  } else {
+    printf("Reading '%s'...\n", input);
+  }
+  fflush(stdout);
 #endif
 
-  saxs_document *doc = saxs_document_create();
-  int result = saxs_document_read(doc, input, format);
+  int rc = 0;
+  struct line *lines = NULL;
+
+  rc = lines_read(&lines, input);
+  if (rc) {
 #ifdef __AFL_HAVE_MANUAL_CONTROL
-  saxs_document_free(doc);
-  }
+    continue;
 #else
-  printf("Result: %d (%s)\n", result, strerror(result));
-  if (result == 0) {
-    printf("Format ID: '%s'\n", saxs_document_format_id(doc));
-    printf("Number of properties: %d\n", saxs_document_property_count(doc));
-    printf("Number of curves: %d\n", saxs_document_curve_count(doc));
+    printf("lines_read returned %d (%s)\n", rc, strerror(rc));
+    return 0;
+#endif
   }
-  saxs_document_free(doc);
+  assert(lines);
+
+  struct saxs_document_format const *fmt = fmt1;
+  while (fmt) {
+    saxs_document *doc = saxs_document_create();
+    assert(doc);
+    if (fmt->read) {
+#ifndef __AFL_HAVE_MANUAL_CONTROL
+      printf("Trying format '%s'...\n", fmt->name);
+      fflush(stdout);
+#endif
+      rc = fmt->read(doc, lines, NULL);
+#ifndef __AFL_HAVE_MANUAL_CONTROL
+      printf("Result: %d (%s)\n", rc, strerror(rc));
+      if (rc == 0) {
+        printf("Number of properties: %d\n", saxs_document_property_count(doc));
+        printf("Number of curves: %d\n", saxs_document_curve_count(doc));
+      }
+#endif
+    }
+    saxs_document_free(doc);
+    if (format) {
+      fmt = saxs_document_format_find_next(fmt, input, format);
+    } else {
+      fmt = saxs_document_format_next(fmt);
+    }
+  }
+
+  lines_free(lines);
+#ifdef __AFL_HAVE_MANUAL_CONTROL
+  }
 #endif
 
   return 0;
