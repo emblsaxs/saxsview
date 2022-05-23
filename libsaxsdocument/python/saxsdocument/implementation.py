f8c2af11ada960c320942eda262aa2c121865f72
Daniel Franke <dfranke76@users.noreply.github.com>
2022-05-23 09:08:54 +0200

    2022-05-23  Daniel Franke  <franke@embl-hamburg.de>
    	* python/saxsdocument/implementation.py: Fixed document type names
    	and pass a column of floats when writing .fir files.

--- libsaxsdocument/python/saxsdocument/implementation.py
+++ libsaxsdocument/python/saxsdocument/implementation.py
@@ -3,7 +3,7 @@ import saxsdocument.api as api
 
 
 def read_dat(filename):
-  doc = api.read(filename, "atsas-3-column-dat");
+  doc = api.read(filename, "atsas-dat-3-column");
   s, I, err = doc.curve(0).data()
   properties = doc.properties()
   return s, I, err, properties
@@ -12,11 +12,11 @@ def write_dat(filename, s, I, err, properties):
   doc = api.create()
   doc.add_curve(s, I, err)
   doc.add_properties(properties)
-  doc.write(filename, "atsas-3-column-dat")
+  doc.write(filename, "atsas-dat-3-column")
 
 
 def read_fir(filename):
-  doc = api.read(filename, "atsas-4-column-fir");
+  doc = api.read(filename, "atsas-fir-4-column");
   s0, I0, err0 = doc.curve(0).data()
   s1, I1, err1 = doc.curve(1).data()
   properties = doc.properties()
@@ -25,7 +25,7 @@ def read_fir(filename):
 def write_fir(filename, s, Iexp, errExp, Ifit, properties):
   doc = api.create()
   doc.add_curve(s, Iexp, errExp)
-  doc.add_curve(s, Ifit, [0] * len(errExp))
+  doc.add_curve(s, Ifit, [0.0] * len(errExp))
   doc.add_properties(properties)
-  doc.write(filename, "atsas-4-column-fir")
+  doc.write(filename, "atsas-fir-4-column")
 
